56ec72c99156c609f601ac7d4856a4d8dc071fab
viresh rawool <vireshrawool007@gmail.com>
2021-02-09 12:44:29 +0530

    added the logic to claculate the length of line

--- line_comparison_problem.java
+++ line_comparison_problem.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 public class line_comparison_problem {
         public static void main(String[] args) {
-		System.out.println("length1 is greater than length2");
+		Scanner myobj = new Scanner(System.in);
+		System.out.println("enter x1 and y1 values");
+		int x1 = myobj.nextInt();
+		int y1 = myobj.nextInt();
+		System.out.println("enter x2 and y2 values");
+                int x2 = myobj.nextInt();
+                int y2 = myobj.nextInt();
+		double length = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+
+		System.out.println( length);
         }
 }
