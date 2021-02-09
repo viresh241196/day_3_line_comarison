f4f6aa24dc88f24a2fb187173f5e7d72fcf98f1d
viresh rawool <vireshrawool007@gmail.com>
2021-02-09 12:47:51 +0530

    modified: logic to compare the 2 lengths is equal or not

--- line_comparison_problem.java
+++ line_comparison_problem.java
@@ -8,8 +8,21 @@ public class line_comparison_problem {
 		System.out.println("enter x2 and y2 values");
                 int x2 = myobj.nextInt();
                 int y2 = myobj.nextInt();
-		double length = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+		double length1 = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+		System.out.println("length1: "+length1);
 
-		System.out.println( length);
+
+		System.out.println("enter a1 and b1 values");
+                int a1 = myobj.nextInt();
+                int b1 = myobj.nextInt();
+                System.out.println("enter a2 and b2 values");
+                int a2 = myobj.nextInt();
+                int b2 = myobj.nextInt();
+		double length2 = Math.sqrt(Math.pow(a2-a1,2)+Math.pow(b2-b1,2));
+		System.out.println("length2: "+length2);
+		if ( length1==length2)
+			System.out.println("length is equal");
+		else
+			System.out.println("length is not equal");
         }
 }
