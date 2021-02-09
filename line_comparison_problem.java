0bca4d1435c4a30f5c92a3128f811f95c6204171
viresh rawool <vireshrawool007@gmail.com>
2021-02-09 11:39:02 +0530

    added the line comparison problem file

---
+++ line_comparison_problem.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class line_comparison_problem {
+        public static void main(String[] args) {
+		System.out.println("length1 is greater than length2");
+        }
+}
